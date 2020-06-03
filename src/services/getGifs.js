fbeb6d566e5c17621eed1135442ebf7591849d18
Alejandro Gutierrez <alejandrogutbarce@gmail.com>
2020-06-03 16:45:33 +0200

    (chore) Change setting file for .env

--- src/services/getGifs.js
+++ src/services/getGifs.js
@@ -1,22 +1,23 @@
-import {API_KEY, API_URL} from './settings'
+const baseUrl = process.env.REACT_APP_API_URL;
+const apiKey = process.env.REACT_APP_API_KEY;
 
 const fromApiResponseToGifs = apiResponse => {
-  const {data = []} = apiResponse
+  const { data = [] } = apiResponse;
   if (Array.isArray(data)) {
     const gifs = data.map(image => {
-      const {images, title, id} = image
-      const { url } = images.downsized_medium
-      return { title, id, url }
-    })
-    return gifs
+      const { images, title, id } = image;
+      const { url } = images.downsized_medium;
+      return { title, id, url };
+    });
+    return gifs;
   }
-  return []
-}
+  return [];
+};
 
-export default function getGifs ({limit = 25, keyword = 'morty', page = 0} = {}) {
-  const apiURL = `${API_URL}/gifs/search?api_key=${API_KEY}&q=${keyword}&limit=${limit}&offset=${page * limit}&rating=G&lang=en`
+export default function getGifs ({ limit = 25, keyword = 'morty', page = 0 } = {}) {
+  const apiURL = `${baseUrl}/gifs/search?api_key=${apiKey}&q=${keyword}&limit=${limit}&offset=${page * limit}&rating=G&lang=en`;
 
   return fetch(apiURL)
     .then(res => res.json())
-    .then(fromApiResponseToGifs)
+    .then(fromApiResponseToGifs);
 }
--- src/services/getTrendingTermsService.js
+++ src/services/getTrendingTermsService.js
@@ -1,14 +1,15 @@
-import {API_KEY, API_URL} from './settings'
+const baseUrl = process.env.REACT_APP_API_URL;
+const apiKey = process.env.REACT_APP_API_KEY;
 
 const fromApiResponseToGifs = apiResponse => {
-  const {data = []} = apiResponse
-  return data
-}
+  const { data = [] } = apiResponse;
+  return data;
+};
 
 export default function getTrendingTerms () {
-  const apiURL = `${API_URL}/trending/searches?api_key=${API_KEY}`
+  const apiURL = `${baseUrl}/trending/searches?api_key=${apiKey}`;
 
   return fetch(apiURL)
     .then(res => res.json())
-    .then(fromApiResponseToGifs)
+    .then(fromApiResponseToGifs);
 }
