c97722d85eeac4a0d963cd2828fce788b31b3523
Alejandro Gutierrez <alejandrogutbarce@gmail.com>
2020-06-04 15:23:20 +0200

    (feat)Lazy load pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import './App.css';
-import Home from './pages/Home';
-import SearchResults from './pages/SearchResults';
-import Detail from './pages/Detail';
-import Context from './context/StaticContext';
-import { GifsContextProvider } from './context/GifsContext';
+import React, { Suspense } from 'react';
 import { Link, Route } from "wouter";
+import './App.css';
+import { GifsContextProvider } from './context/GifsContext';
+import Context from './context/StaticContext';
+
+const HomePage = React.lazy(() => import('./pages/Home'));
+const DetailPage = React.lazy(() => import('./pages/Detail'));
+const SearchResultsPage = React.lazy(() => import('./pages/SearchResults'));
 
 export default function App () {
   return (
@@ -14,26 +15,28 @@ export default function App () {
       suscribeteAlCanal: true
     }}>
       <div className="App">
-        <section className="App-content">
-          <Link to="/">
-            <figure className="App-logo">
-              <img alt='Giffy logo' src='/logo.png' />
-            </figure>
-          </Link>
-          <GifsContextProvider>
-            <Route
-              component={Home}
-              path="/"
-            />
-            <Route
-              component={SearchResults}
-              path="/search/:keyword" />
-            <Route
-              component={Detail}
-              path="/gif/:id"
-            />
-          </GifsContextProvider>
-        </section>
+        <Suspense fallback={null}>
+          <section className="App-content">
+            <Link to="/">
+              <figure className="App-logo">
+                <img alt='Giffy logo' src='/logo.png' />
+              </figure>
+            </Link>
+            <GifsContextProvider>
+              <Route
+                component={HomePage}
+                path="/"
+              />
+              <Route
+                component={SearchResultsPage}
+                path="/search/:keyword" />
+              <Route
+                component={DetailPage}
+                path="/gif/:id"
+              />
+            </GifsContextProvider>
+          </section>
+        </Suspense>
       </div>
     </Context.Provider>
   );
